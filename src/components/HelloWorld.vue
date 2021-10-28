9c084dcc26458d35eb5a2e0b1b9487008f4081a3
Leandro <leandro83g@icloud.com>
2021-10-28 23:49:13 +0900

    add prettierrc

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -95,7 +95,7 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
+import { Options, Vue } from 'vue-class-component';
 
 @Options({
   props: {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <MDBBtn color="primary">Primary</MDBBtn>
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { MDBBtn } from 'mdb-vue-ui-kit';
 
 @Options({
   components: {
     HelloWorld,
+    MDBBtn,
   },
 })
 export default class Home extends Vue {}
